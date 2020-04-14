09b37835f730a499dafb7db7e03dc2e14ebae057
Maikel Llamaret Heredia <llamaret@webmisolutions.com>
2020-04-14 21:24:10 +0000

    Update update_countries.py

--- utils/update_countries.py
+++ utils/update_countries.py
@@ -12,27 +12,35 @@ def change_date(dat):
 def get_json_info():
     countries = requests.get('https://pomber.github.io/covid19/timeseries.json').json()
     inf =  {}
+    back_inf = {}
     fecha = '2020-1-22'
     for c in countries:
         data = []
+        deaths = []
+        recovered = []
         started = False
         for day in countries[c]:
             if not started:
                 if day['confirmed']!=0:
                     started = True
                     data.append(day['confirmed'])
+                    deaths.append(day['deaths'])
+                    recovered.append(day['recovered'])
                     fecha = day["date"]
             else:
                 data.append(day['confirmed'])
+                deaths.append(day['deaths'])
+                recovered.append(day['recovered'])               
                 fecha = day["date"]
-        inf[c] = data
-    c = {"paises": inf, "dia-actualizacion": change_date(fecha)}
+        back_inf[c] = data
+        inf[c] = {'confirmed':data,'deaths':deaths,'recovered':recovered}
+    c = {"paises":back_inf,"paises_info": inf, "dia-actualizacion": change_date(fecha)}
     return c
 
 def main():
 
     data = get_json_info()
-    path = 'assets/data/paises-info-dias.json'
+    path = 'data/paises-info-dias.json'
     json.dump(data, open(path, 'w'))
 
     print(json.dumps(data, indent=2))
