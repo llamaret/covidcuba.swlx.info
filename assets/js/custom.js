7c06bd01f1aac8f4d9e6588115332722d43ee6e8
Maikel Llamaret Heredia <llamaret@webmisolutions.com>
2020-04-18 21:23:26 -0400

    Fixing header

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -220,23 +220,23 @@ province_order = {
 }
 
 var population = {
-	'cuba': 11209628,
-	'21': 588555,//PRI
-	'22': 511079,//ART
-	'23': 2131480,//LHA
-	'24': 383043,//MAY
-	'25': 714843,//MAT
-	'26': 780749,//VCL
-	'27': 406751,//CFG
-	'28': 465780,//SSP
-	'29': 435006,//CAV
-	'30': 767138,//CAM
-	'31': 535335,//LTU
-	'32': 1027249,//HOL
-	'33': 823651,//GRA
-	'34': 1049256,//STG
-	'35': 508552,//GTM
-	'40.01': 83801,//IJV	
+    'cuba': 11209628,
+    '21': 588555,//PRI
+    '22': 511079,//ART
+    '23': 2131480,//LHA
+    '24': 383043,//MAY
+    '25': 714843,//MAT
+    '26': 780749,//VCL
+    '27': 406751,//CFG
+    '28': 465780,//SSP
+    '29': 435006,//CAV
+    '30': 767138,//CAM
+    '31': 535335,//LTU
+    '32': 1027249,//HOL
+    '33': 823651,//GRA
+    '34': 1049256,//STG
+    '35': 508552,//GTM
+    '40.01': 83801,//IJV	
 }
 
 $.ajaxSetup({cache: false});
@@ -270,6 +270,11 @@ $.walker = {
     },
 
     view: {
+        addOptionToSelect: function (select, value, text) {
+            if ($(select).find('option[value="' + value + '"]').length)
+                return;
+            $(select).append('<option value="' + value + '">' + text + '</option>')
+        },
         update: function () {
             $cards.show();
             $selector.show();
@@ -330,7 +335,7 @@ $.walker = {
                 }*/
                 if ($('#proscurve-select1').find('option[value="' + province.DPA_province_code + '"]').length === 0 && province.province !== 'Desconocida') {
                     sorteddata.push($.walker.province.list.features[i].properties);
-                    $('#proscurve-select1').append('<option value="' + province.DPA_province_code + '">' + province.province + '</option>');
+                    $.walker.view.addOptionToSelect('#proscurve-select1', province.DPA_province_code, province.province);
                 }
                 remaining[$.walker.province.list.features[i].properties.DPA_province_code] = {"total": 0};
             }
@@ -345,9 +350,9 @@ $.walker = {
             });
             for (var j = 0; j < sorteddata.length; j++) {
                 const province2 = sorteddata[j];
-                $target.append('<option value="' + province2.province_id + '">' + province2.province + '</option>');
-                $('#proscurve-select1').append('<option value="' + province2.DPA_province_code + '">' + province2.province + '</option>');
-                $('#proscurve-select2').append('<option value="' + province2.DPA_province_code + '">' + province2.province + '</option>');
+                $.walker.view.addOptionToSelect($target, province2.province_id, province2.province);
+                $.walker.view.addOptionToSelect('#proscurve-select1', province2.DPA_province_code, province2.province);
+                $.walker.view.addOptionToSelect('#proscurve-select2', province2.DPA_province_code, province2.province);
             }
             return remaining;
         },
@@ -472,6 +477,7 @@ function run_calculations() {
                         var sex_female = 0;
                         var sex_unknown = 0;
                         var casos_hoy = 0;
+                        var casos_ayer = 0;
                         var sujetos_riesgo = 0;
                         var recuperados_hoy = 0;
                         var graves_numero = 0;
@@ -605,7 +611,7 @@ function run_calculations() {
                                 }
                             }
                         }
-
+                        casos_ayer = data.casos.dias[cantidad_dias - 1]["diagnosticados"].length;
                         //Bar for countries
                         var country = ['País'];
                         var countryDiagnoses = ['Diagnosticados'];
@@ -844,7 +850,7 @@ function run_calculations() {
                         // Por ciento de Tests Positivos en el Día y Acumulado
 
                         for (var i = 1; i < test_days.length; i++) {
-                            dailyPorcientoPositivo.push(((test_positive[i] - test_positive[i - 1])*100.0/(test_cases[i] - test_cases[i - 1])).toFixed(2));
+                            dailyPorcientoPositivo.push(((test_positive[i] - test_positive[i - 1]) * 100.0 / (test_cases[i] - test_cases[i - 1])).toFixed(2));
                             dailyPorcientoPositivoAcumulado.push((test_positive[i] * 100.0 / test_cases[i]).toFixed(2));
                         }
 
@@ -921,13 +927,13 @@ function run_calculations() {
                                     c_temp.push(countriesdays.paises_info[c].confirmed[i]);
                                     c_r_temp.push(countriesdays.paises_info[c].recovered[i]);
                                     c_d_temp.push(countriesdays.paises_info[c].deaths[i]);
-                                    c_a_temp.push(countriesdays.paises_info[c].confirmed[i]-
-                                                countriesdays.paises_info[c].deaths[i]-
-                                                countriesdays.paises_info[c].recovered[i]);
-                                    if(i>0){
-                                        c_day_temp.push(countriesdays.paises_info[c].confirmed[i]-
-                                                    countriesdays.paises_info[c].confirmed[i-1]);
-                                    }else{
+                                    c_a_temp.push(countriesdays.paises_info[c].confirmed[i] -
+                                        countriesdays.paises_info[c].deaths[i] -
+                                        countriesdays.paises_info[c].recovered[i]);
+                                    if (i > 0) {
+                                        c_day_temp.push(countriesdays.paises_info[c].confirmed[i] -
+                                            countriesdays.paises_info[c].confirmed[i - 1]);
+                                    } else {
                                         c_day_temp.push(countriesdays.paises_info[c].confirmed[i]);
                                     }
                                     d_temp.push('Día ' + (i + 1));
@@ -958,7 +964,7 @@ function run_calculations() {
                             countryselected = val;
                             //comparison.load({columns: [curves[countryselected]['data']]});
                             //curve.load({columns: [curves[countryselected]['data']]});
-                            if(tab_selected=='confirmados'){
+                            if (tab_selected == 'confirmados') {
                                 comparison = c3.generate({
                                     bindto: "#countries-comparison-confirmados",
                                     data: {
@@ -1018,7 +1024,7 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(tab_selected=='recuperados'){
+                            if (tab_selected == 'recuperados') {
                                 comparison_recover = c3.generate({
                                     bindto: "#countries-comparison-recuperados",
                                     data: {
@@ -1078,7 +1084,7 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(tab_selected=='fallecidos'){
+                            if (tab_selected == 'fallecidos') {
                                 comparison_death = c3.generate({
                                     bindto: "#countries-comparison-fallecidos",
                                     data: {
@@ -1138,7 +1144,7 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(tab_selected=='activos'){
+                            if (tab_selected == 'activos') {
                                 comparison_active = c3.generate({
                                     bindto: "#countries-comparison-activos",
                                     data: {
@@ -1198,7 +1204,7 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(tab_selected=='diarios'){
+                            if (tab_selected == 'diarios') {
                                 comparison_daily = c3.generate({
                                     bindto: "#countries-comparison-diarios",
                                     data: {
@@ -1264,8 +1270,8 @@ function run_calculations() {
                             e.target // newly activated tab
                             e.relatedTarget // previous active tab
                             let reff = e.target.attributes.href.value;
-                            if(reff=='#tab-confirmados'){
-                                tab_selected='confirmados';
+                            if (reff == '#tab-confirmados') {
+                                tab_selected = 'confirmados';
                                 comparison = c3.generate({
                                     bindto: "#countries-comparison-confirmados",
                                     data: {
@@ -1325,8 +1331,8 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(reff=='#tab-recuperados'){
-                                tab_selected='recuperados';
+                            if (reff == '#tab-recuperados') {
+                                tab_selected = 'recuperados';
                                 comparison_recover = c3.generate({
                                     bindto: "#countries-comparison-recuperados",
                                     data: {
@@ -1386,8 +1392,8 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(reff=='#tab-fallecidos'){
-                                tab_selected='fallecidos';
+                            if (reff == '#tab-fallecidos') {
+                                tab_selected = 'fallecidos';
                                 comparison_death = c3.generate({
                                     bindto: "#countries-comparison-fallecidos",
                                     data: {
@@ -1447,8 +1453,8 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(reff=='#tab-activos'){
-                                tab_selected='activos';
+                            if (reff == '#tab-activos') {
+                                tab_selected = 'activos';
                                 comparison_active = c3.generate({
                                     bindto: "#countries-comparison-activos",
                                     data: {
@@ -1508,8 +1514,8 @@ function run_calculations() {
                                     }
                                 });
                             }
-                            if(reff=='#tab-diarios'){
-                                tab_selected='diarios';
+                            if (reff == '#tab-diarios') {
+                                tab_selected = 'diarios';
                                 comparison_daily = c3.generate({
                                     bindto: "#countries-comparison-diarios",
                                     data: {
@@ -1946,7 +1952,7 @@ function run_calculations() {
                             }
                         });
 
-                        return {"cases": cases, "deaths": deaths, "gone": gone, "recov": recov, "female": sex_female, "male": sex_male, "unknownsex": sex_unknown, "casos_hoy": casos_hoy, "sujetos_riesgo": sujetos_riesgo, "recuperados_hoy": recuperados_hoy, "graves_numero": graves_numero, "muertes_hoy": muertes_hoy, "cantidad_dias": cantidad_dias};
+                        return {"cases": cases, "deaths": deaths, "gone": gone, "recov": recov, "female": sex_female, "male": sex_male, "unknownsex": sex_unknown, "casos_hoy": casos_hoy, "casos_ayer": casos_ayer, "sujetos_riesgo": sujetos_riesgo, "recuperados_hoy": recuperados_hoy, "graves_numero": graves_numero, "muertes_hoy": muertes_hoy, "cantidad_dias": cantidad_dias};
                     }
 
                     var globalInfo = getAllCasesAndSimpleGraphics();
@@ -1978,6 +1984,7 @@ function run_calculations() {
                             "female": globalInfo.female,
                             "sexunknown": globalInfo.sex_unknown,
                             "casos_hoy": globalInfo.casos_hoy,
+                            "casos_ayer": globalInfo.casos_ayer,
                             "sujetos_riesgo": globalInfo.sujetos_riesgo,
                             "recuperados_hoy": globalInfo.recuperados_hoy,
                             "graves_numero": globalInfo.graves_numero,
@@ -2047,7 +2054,12 @@ function run_calculations() {
                     $('[data-content=recupe]').html(genInfo.recov ? genInfo.recov : '-');                    
                     $("[data-content=mortalidad]").html(((genInfo.deaths * 100) / genInfo.total).toFixed(2));
                     $("[data-content=recuperacion]").html(((genInfo.recov * 100) / genInfo.total).toFixed(2));
-                    $('[data-content=diagno_hoy]').html(genInfo.casos_hoy);
+                    if(genInfo.casos_hoy < genInfo.casos_ayer) {
+                    $('[data-content=diagno_hoy]').html(genInfo.casos_hoy + "(-" + (genInfo.casos_ayer - genInfo.casos_hoy) + ")");
+                    }
+                    else {
+                    $('[data-content=diagno_hoy]').html(genInfo.casos_hoy + "(+" + (genInfo.casos_hoy - genInfo.casos_ayer) + ")");
+                    }
                     $('[data-content=ingresados]').html(genInfo.sujetos_riesgo);
                     $('[data-content=recupe_hoy]').html(genInfo.recuperados_hoy);
                     $('[data-content=graves]').html(genInfo.graves_numero);
